6582c9bc8cb389ff70f568288437cfc2a168fad2
10756 <lisongyan7050@gmail.com>
2024-09-06 10:15:35 +0800

    添加了点注释

--- core/src/main/java/org/lisongyan/rpc/core/annotation/EnableRpc.java
+++ core/src/main/java/org/lisongyan/rpc/core/annotation/EnableRpc.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
 
+
+/**
+ * 这是一个启动rpc的注解，这个注解需要加在springboot的启动类上（其他地方也行，主要是为了便于查看）
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 @Documented
